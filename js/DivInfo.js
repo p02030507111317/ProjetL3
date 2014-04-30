41eab2575346f1fbf075e7674786458131c4a55f
François <francois.caillet@gmail.com>
2014-04-30 18:47:28 +0200

    Update DivInfo.js
    Modification/Ajout:
    • Modification de l'affichage des divInfos global {8;13}
    • Les divs de s'affiche plus dorénavant en haut à droite et en haut à gauche mais leur position varie en fonction du champ survolé {15,31} (surement sale, à amélioré..)

--- js/DivInfo.js
+++ js/DivInfo.js
@@ -1,14 +1,34 @@
-function displayDivInfo(text){
+function displayDivInfo(text, number){
 	
     if(text){
          
         //Création d'une div provisoire
         var divInfo = document.createElement('div');
         divInfo.style.position = 'absolute';
-        divInfo.style.left = 0 + 'px';
-        divInfo.style.top = 0+ 'px';
-        divInfo.style.backgroundColor = 'blue';
-        divInfo.style.color='white';
+        divInfo.style.backgroundColor = '#f5f5f5';
+        divInfo.style.color='#555';
+        divInfo.style.zIndex='11111';
+        divInfo.style.borderRadius= 3 + 'px';
+        divInfo.style.boxShadow= '3px 5px 10px rgba(0,0,0,0.3)';
+        divInfo.style.transition= '800ms ease-out';
+
+        if (number=='1') {
+            divInfo.style.left = 58 + '%';
+            divInfo.style.top = 16+ '%';
+
+        } else if (number=='2') {
+            divInfo.style.left = 62 + '%';
+            divInfo.style.top = 30+ '%';
+
+        }else if (number=='3') {
+            divInfo.style.left = 65 + '%';
+            divInfo.style.top = 38+ '%';
+
+        }else if (number=='4') {
+            divInfo.style.left = 65 + '%';
+            divInfo.style.top = 54+ '%';
+
+        };
 
         document.onmousemove = 
         	divInfo.id = 'divInfo';
